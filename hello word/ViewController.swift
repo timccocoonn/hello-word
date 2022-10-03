1a36079cb55fea0dae76ef0bd5371838bb83a903
@timccocoonn <timi.tf.timi@gmail.com>
2022-10-03 22:19:54 +0300

    Initial Commit

---
+++ hello word/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hello word
+//
+//  Created by Тимофей  Федоренко on 03.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
