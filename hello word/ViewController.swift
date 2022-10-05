3a17b9e88ffb623422d04b9e9e601c44d550f7da
@timccocoonn <timi.tf.timi@gmail.com>
2022-10-05 22:49:19 +0300

    finish maybe

--- hello word/ViewController.swift
+++ hello word/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var helloJediLabel: UILabel!
+    @IBOutlet var toggleButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        helloJediLabel.isHidden = true
+        toggleButton.layer.cornerRadius = 15
+        
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func makeActionWithButton() {
+        if helloJediLabel.isHidden {
+            helloJediLabel.isHidden = false
+            toggleButton.setTitle("Hide text", for: .normal)
+        } else {
+            helloJediLabel.isHidden = true
+            toggleButton.setTitle("Show text", for: .normal)
+        }
+    }
 }
 
